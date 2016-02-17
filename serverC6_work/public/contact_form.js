e8c659868108d0cc62123e4f9972f50c99fc2edc
David Isaza <Davidisaza21@gmail.com>
2016-02-17 18:39:08 -0500

    edits recommended to Alejandra

--- serverC6_work/public/contact_form.js
+++ serverC6_work/public/contact_form.js
@@ -1,9 +1,55 @@
 $(function() {
+  // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
     // Set up an event listener for the contact form.
-  $('#ajax-contact').submit(function(event) {
+  $(form).submit(function(event) {
+
     // Stop the browser from submitting the form.
-      event.preventDefault();
+      // event.preventDefault();
+
   });
+
+  // Serialize the form data.
+    var formData = $(form).serialize();
+
+    // Submit the form using AJAX.
+$.ajax({
+    type: 'POST',
+    url: $(form).attr('action'),
+    data: formData
+})
+
+.done(function(response) {
+    // Make sure that the formMessages div has the 'success' class.
+    $(formMessages).removeClass('error');
+    $(formMessages).addClass('success');
+
+    // Set the message text.
+    $(formMessages).text(response);
+
+    // Clear the form.
+    $('#name').val('');
+    $('#email').val('');
+    $('#message').val('');
+})
+
+.fail(function(data) {
+    // Make sure that the formMessages div has the 'error' class.
+    $(formMessages).removeClass('success');
+    $(formMessages).addClass('error');
+
+    // Set the message text.
+    if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+    } else {
+        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    }
+});
+
   $("#contact-me").click(function(){
     $("#header").load("contact_form.html");
   });
@@ -14,5 +60,5 @@ $(function() {
   $(".exit-button").click(function(){
     $("#header").load(location.href + " #header");
   });
-  
+
 });
